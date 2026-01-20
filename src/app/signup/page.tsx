bbe2a10f858fb8fa201da00a2fd7b064dc378c97
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-01-19 21:07:18 -0300

    feat: add signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import FormSignUp from "./_components/FormSignUp/FormSignUp";
+
+const SignUpPage = () => {
+  return (
+    <div className="min-h-screen bg-app-background flex items-center justify-center px-4">
+      <div className="w-full">
+        <FormSignUp />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
