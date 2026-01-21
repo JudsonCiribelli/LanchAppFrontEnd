778e0a9072c0bd47a77ec2af6f720a41581c9342
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-01-21 13:12:08 -0300

    feat: add user types

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export interface UserTypes {
+  name: string;
+  email: string;
+  phone: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+  addresses: string[];
+  orders: string[];
+}
