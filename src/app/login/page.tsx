68236cf4ea549c279f690eaba28960a04b9956d9
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-01-21 13:11:49 -0300

    chore: ensure the user will not be able to access the login and sginup page if they are already logged in.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,14 @@
+import { getUser } from "@/lib/authToken";
 import LoginForm from "./_components/FormLogin/FormLogin";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const user = await getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="min-h-screen bg-app-background flex items-center justify-center px-4">
       <div className="w-full">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import FormSignUp from "./_components/FormSignUp/FormSignUp";
+import { getUser } from "@/lib/authToken";
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+  const user = await getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="min-h-screen bg-app-background flex items-center justify-center px-4">
       <div className="w-full">
