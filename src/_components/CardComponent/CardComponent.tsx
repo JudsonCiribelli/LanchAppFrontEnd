db6b59b60573755d4f1327eaa94aabc8e12cac36
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-01-22 11:59:52 -0300

    feat:  add navigation to side bar buttons

--- src/_components/CardComponent/CardComponent.tsx
+++ src/_components/CardComponent/CardComponent.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Grid2x2, ShoppingBasket, ShoppingCart } from "lucide-react";
@@ -9,18 +10,34 @@ const CardComponent = () => {
         <CardTitle>Navegue entre as páginas</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col space-y-3 ">
-        <Button className="flex items-start justify-start bg-brand-primary">
-          <ShoppingCart size={24} />
-          <p className="text-sm font-bold">Pedidos</p>
-        </Button>
-        <Button className="flex items-start justify-start bg-brand-primary">
-          <ShoppingBasket size={24} />
-          <p className="text-sm font-bold">Produtos</p>
+        <Button
+          className="flex items-start justify-start bg-brand-primary"
+          asChild
+        >
+          <Link href="/dashboard/orders">
+            <ShoppingCart size={24} />
+            <p className="text-sm font-bold">Pedidos</p>
+          </Link>
         </Button>
 
-        <Button className="flex items-start justify-start bg-brand-primary">
-          <Grid2x2 size={24} />
-          <p className="text-sm font-bold">Pedidos</p>
+        <Button
+          className="flex items-start justify-start bg-brand-primary"
+          asChild
+        >
+          <Link href="/dashboard/products">
+            <ShoppingBasket size={24} />
+            <p className="text-sm font-bold">Produtos</p>
+          </Link>
+        </Button>
+
+        <Button
+          className="flex items-start justify-start bg-brand-primary"
+          asChild
+        >
+          <Link href="/dashboard/categories">
+            <Grid2x2 size={24} />
+            <p className="text-sm font-bold">Categorias</p>
+          </Link>
         </Button>
       </CardContent>
     </Card>
