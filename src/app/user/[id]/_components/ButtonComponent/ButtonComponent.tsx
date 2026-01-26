4438790a40dadf96d8ba37b055bd6e752be112eb
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-01-26 18:45:34 -0300

    feat: add button form component

---
+++ src/app/user/[id]/_components/ButtonComponent/ButtonComponent.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { redirect } from "next/navigation";
+import { Button } from "@/_components/ui/button";
+import { ArrowLeft } from "lucide-react";
+
+const ButtonComponent = () => {
+  const handleRouterUser = () => {
+    redirect("/dashboard");
+  };
+
+  return (
+    <Button className="bg-white text-black" onClick={handleRouterUser}>
+      <ArrowLeft size={22} />
+    </Button>
+  );
+};
+
+export default ButtonComponent;
