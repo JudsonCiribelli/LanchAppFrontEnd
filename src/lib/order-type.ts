dc1c499dd3f23b354ba44cbdfa76531733ca4b3d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-04 16:28:38 -0300

    feat: add order type config

---
+++ src/lib/order-type.ts
@@ -0,0 +1,5 @@
+export const typeConfig: { [key: string]: { label: string; color: string } } = {
+  DELIVERY: { label: "Delivery", color: "bg-yellow-500" },
+  PICKUP: { label: "Retirada", color: "bg-blue-500" },
+  DINE_IN: { label: "Mesa", color: "bg-green-600" },
+};
