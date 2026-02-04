8dd3c8f3995376bd4cffdc2a7d95bc1c851db452
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-04 16:27:47 -0300

    feat: add order status type

---
+++ src/types/orderStatus.ts
@@ -0,0 +1,10 @@
+export type OrderStatus =
+  | "PENDING"
+  | "PAYMENT_CONFIRMED"
+  | "PAYMENT_FAILED"
+  | "IN_PREPARATION"
+  | "READY"
+  | "ON_THE_WAY"
+  | "DELIVERED"
+  | "FINISHED"
+  | "CANCELED";
