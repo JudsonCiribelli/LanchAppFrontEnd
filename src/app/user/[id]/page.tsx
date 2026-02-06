3c8b385d341251446bf0ee885ce240bd3703c1b0
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-06 11:32:45 -0300

    feat: add user profile page

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,14 @@
 "use server";
-import { getUser } from "@/lib/authToken";
+import { getToken, getUser } from "@/lib/authToken";
+import { Button } from "@/_components/ui/button";
+import { Bell, MoveLeft } from "lucide-react";
 import ButtonComponent from "./_components/ButtonComponent/ButtonComponent";
 import AddressesCardComponent from "./_components/AddressesCardComponent/AddressesCardComponent";
+import { getInitials } from "@/_helpers/getInitials";
+import OrdersCardComponent from "./_components/OrdersCardComponent/OrdersCardComponent";
+import { OrderTypes } from "@/types/order";
+import { apiClient } from "@/lib/api";
+import UserCardComponent from "./_components/UserCardComponent/UserCardComponent";
 
 const UserProfile = async () => {
   const user = await getUser();
@@ -10,17 +17,40 @@ const UserProfile = async () => {
     return null;
   }
 
-  return (
-    <main className="h-screen  bg-app-background space-y-4">
-      <div className="px-3 py-2 flex items-center gap-2 xl:p-10">
-        <ButtonComponent />
-        <h1 className="text-white text-xl">Perfil do Usuário</h1>
-      </div>
+  const token = await getToken();
 
-      <div className="xl:items-center justify-center flex w-full flex-col mt-10s">
-        <AddressesCardComponent user={user} />
-      </div>
-    </main>
+  const orders = await apiClient<OrderTypes[]>("/user/orders", {
+    method: "GET",
+    cache: "no-store",
+    token: token,
+  });
+
+  return (
+    <div className="min-h-screen  bg-app-background text-slate-200 p-6 md:p-12">
+      <header className="max-w-6xl mx-auto mb-8 flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <ButtonComponent />
+          <h1>Perfil do Usuário</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <Bell size={24} />
+
+          <Button className="w-13 h-13 bg-blue-600 rounded-full flex items-center justify-center text-2xl font-bold text-white shadow-lg border-4 border-slate-800 cursor-pointer hover:bg-blue-500">
+            <p className="">{getInitials(user.name)}</p>
+          </Button>
+        </div>
+      </header>
+      <main className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="col-span-1">
+          <UserCardComponent user={user} />
+        </div>
+
+        <div className="col-span-1 md:col-span-2 space-y-6">
+          <AddressesCardComponent user={user} />
+          {user.orders.length > 0 && <OrdersCardComponent orders={orders} />}
+        </div>
+      </main>
+    </div>
   );
 };
 
