9a9006bb76e02d855f4b4e090cdb1a597f126150
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-09 17:00:38 -0300

    feat: add produtos type to category types

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,6 +1,9 @@
+import { ProductsType } from "@/app/dashboard/products/types/products";
+
 export interface CategoryTypes {
   id: string;
   name: string;
   createdAt: string;
+  products?: ProductsType[];
   updatedAt: string;
 }
