34514ff9194c32eca7f16b4a1b85a785d16bcf3f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-09 17:01:07 -0300

    feat: add updated form schema

--- src/app/user/[id]/schemas/schema.ts
+++ src/app/user/[id]/schemas/schema.ts
@@ -16,3 +16,20 @@ export const AddressesFormSchema = z.object({
 });
 
 export type AddressesFormType = z.infer<typeof AddressesFormSchema>;
+
+export const UpdatedAddressesSchema = z.object({
+  newStreet: z.string().nonempty({ message: "Campo obrigatorio!" }),
+  newNumber: z.string(),
+  newComplement: z.string().nonempty({ message: "Campo obrigatorio!" }),
+  newNeighborhood: z.string().nonempty({ message: "Campo obrigatorio!" }),
+  newCity: z.string().nonempty({ message: "Campo obrigatorio!" }),
+  newState: z.string().nonempty({ message: "Campo obrigatorio!" }),
+  newZipCode: z
+    .string()
+    .transform((val) => val.replace(/\D/g, ""))
+    .refine((val) => val.length === 8, {
+      message: "Campo obrigatorio!",
+    }),
+});
+
+export type UpdatedAddressesType = z.infer<typeof UpdatedAddressesSchema>;
